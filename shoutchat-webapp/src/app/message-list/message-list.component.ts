f8e493b199f655cade9dd4ea92211a0e4747104d
Dhruvjit <idhruvjit@gmail.com>
2019-03-17 17:29:34 +0100

    shoutchat-webapp: unfinished typescript class import for chat window list

--- shoutchat-webapp/src/app/message-list/message-list.component.ts
+++ shoutchat-webapp/src/app/message-list/message-list.component.ts
@@ -9,7 +9,7 @@ import { Message } from '../models/message';
 export class MessageListComponent implements OnInit {
 
   @Input('messages')
-  private messages : Message[];
+  public messages : Message[];
   
   constructor() { }
 
